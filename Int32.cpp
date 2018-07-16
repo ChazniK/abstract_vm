66385ca698a688151d6b68f921e490b5995d5695
Chazni KATZ <ckatz@e4r19p5.wethinkcode.co.za>
2018-07-16 15:22:36 +0200

    Updated Int32, conforms to Coplien form

--- Int32.cpp
+++ Int32.cpp
@@ -6,7 +6,7 @@
 /*   By: ckatz <ckatz@student.wethinkcode.co.za>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/29 16:03:19 by ckatz             #+#    #+#             */
-/*   Updated: 2018/07/15 22:38:45 by ckatz            ###   ########.fr       */
+/*   Updated: 2018/07/16 15:21:29 by ckatz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 
 Int32::Int32(void)
 {
-	std::cout << "Int32 constructor called" << std::endl;
+
 }
 
 Int32::Int32(const std::string numAsString, eOperandType type)
 {
 	long double t = stold(numAsString);
+
 	try
 	{
 		if (t > INT32_MAX)
@@ -48,17 +49,11 @@ Int32::Int32(const std::string numAsString, eOperandType type)
 	int32_t temp = static_cast<int32_t>(t);
 	this->_value = std::to_string(temp);
 	this->_type = type;
-	// std::cout << "Constructor with of type: " << type << " value: " << numAsString << std::endl;
-}
-
-Int32::Int32(Int32 const & src)
-{
-	std::cout << "Copy constructor with of: " << src.toString() << " type: " << src.getType() << std::endl;
 }
 
 Int32::~Int32(void)
 {
-	std::cout << "Int32 destructor called" << std::endl;
+
 }
 
 int	Int32::getPrecision(void) const
@@ -129,15 +124,4 @@ IOperand const * Int32::operator%(IOperand const & rhs) const
 	long double b = std::stold(rhs.toString());
 	newType =  (getPrecision() > rhs.getPrecision()) ? getType() : rhs.getType();
 	return fact.createOperand(newType, std::to_string(std::fmod(a, b)));
-}
-
-Int32 & Int32::operator=(Int32 const & src)
-{
-	std::cout << "Int32 copy constructor called" << std::endl;
-	if (this != &src)
-	{
-		this->_value = src.toString();
-		this->_type = src.getType();
-	}
-	return *this;
 }
